2b62848e604cea550b07bb96be3eb2ca8e5bafd0
Itunuloluwa Fatoki <itunuworks@gmail.com>
2020-03-30 15:03:22 +0100

    create a sample blue environment - modify the output of product

---
+++ blue-green.tf
@@ -0,0 +1,11 @@
+module "web_server_blue" {
+  source = "git@github.com:ifatoki/web-server-terraform.git?ref=develop"
+
+  vpc_route_table_id = aws_vpc.default.main_route_table_id
+  elb_sg_id = aws_security_group.elb.id
+  default_sg_id = aws_security_group.default.id
+  default_ig_id = aws_internet_gateway.default.id
+  default_subnet_id = aws_subnet.default.id
+  key_id = aws_key_pair.auth.id
+  aws_region = var.aws_region
+}
--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,3 @@
-output "vpc_route_table_id" {
-  value = aws_vpc.default.main_route_table_id
-}
-
-output "elb_sg_id" {
-  value = aws_security_group.elb.id
-}
-
-output "default_sg_id" {
-  value = aws_security_group.default.id
-}
-
-output "key_id" {
-  value = aws_key_pair.auth.id
-}
-
-output "default_subnet_id" {
-  value = aws_subnet.default.id
-}
-
-output "default_ig_id" {
-  value = aws_internet_gateway.default.id
+output "blue_address" {
+  value = module.web_server_blue.address
 }
